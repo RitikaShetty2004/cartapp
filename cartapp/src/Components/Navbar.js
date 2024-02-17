cb6335fe43d9dc213572726d21e67c0c5d521902
RitikaShetty2004 <ritikashetty2004@gmail.com>
2024-02-17 20:33:22 +0530

    git

--- cartapp/src/Components/Navbar.js
+++ cartapp/src/Components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark custom-navbar-purple">
             <div className="container">
-                <a className="navbar-brand" href="/">Shopzy</a>
+                <a className="navbar-brand" href="/">ShopIt</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
